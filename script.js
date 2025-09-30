2d3ea80996dd818a7c1a1eeb69cc64b8a8f14fd0
Srivathsav M <srivathsav.m.2001@gmail.com>
2025-09-30 10:46:25 +0530

    Added Transform

--- script.js
+++ script.js
@@ -6,11 +6,40 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+const group = new THREE.Group()
+group.scale.y = 2
+// group.rotation.y = Math.PI * 0.25
+group.rotation.x = Math.PI * 0.25
+scene.add(group)
+
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+mesh.position.x = 1
+group.add(mesh)
+
+const material2 = new THREE.MeshBasicMaterial({ color: 0xff00ff })
+const mesh2 = new THREE.Mesh(geometry, material2)
+// mesh2.position.x = 0
+group.add(mesh2)
+
+const material3 = new THREE.MeshBasicMaterial({ color: 0x00ffff })
+const mesh3 = new THREE.Mesh(geometry, material3)
+mesh3.position.x = 1
+mesh3.position.z = -1
+group.add(mesh3)
+
+const material4 = new THREE.MeshBasicMaterial({ color: 0xffff00 })
+const mesh4 = new THREE.Mesh(geometry, material4)
+mesh4.position.x = -1
+mesh4.position.z = -1
+group.add(mesh4)
+
+const material5 = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const mesh5 = new THREE.Mesh(geometry, material5)
+mesh5.position.x = -1
+group.add(mesh5)
 
 // Sizes
 const sizes = {
@@ -21,6 +50,7 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+// camera.lookAt(new THREE.Vector3(1, 0, 0))
 scene.add(camera)
 
 // Renderer
@@ -28,5 +58,9 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
 renderer.render(scene, camera)
 
