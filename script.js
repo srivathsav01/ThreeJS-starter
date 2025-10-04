b1c9abce56c1129dae2eed54dda652b686053511
Srivathsav M <srivathsav.m.2001@gmail.com>
2025-10-04 12:29:23 +0530

    Added Camera Controls

--- script.js
+++ script.js
@@ -1,56 +1,82 @@
 import * as THREE from 'three'
 import { gsap } from 'gsap'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
+
+// const cursor = {
+//     x: 0,
+//     y: 0
+// }
+
+// window.addEventListener('mousemove', (event) =>
+// {
+//     cursor.x = event.clientX / sizes.width - 0.5
+//     cursor.y = -(event.clientY / sizes.height - 0.5)
+//     // console.log(cursor.x, cursor.y)
+// })
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix() 
+    // When we change camera properties you need to update projection matrix
+    
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 // Scene
 const scene = new THREE.Scene()
 
 const group = new THREE.Group()
 group.scale.y = 1
-// group.rotation.y = Math.PI * 0.25
 group.rotation.x = Math.PI * 0.25
 scene.add(group)
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const geometry = new THREE.BoxGeometry(1, 1, 1,2,2,2)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
 mesh.position.x = 1
 group.add(mesh)
 
-const material2 = new THREE.MeshBasicMaterial({ color: 0xff00ff })
+const material2 = new THREE.MeshBasicMaterial({ color: 0xff00ff, wireframe: true })
 const mesh2 = new THREE.Mesh(geometry, material2)
-// mesh2.position.x = 0
 group.add(mesh2)
 
-const material3 = new THREE.MeshBasicMaterial({ color: 0x00ffff })
+const material3 = new THREE.MeshBasicMaterial({ color: 0x00ffff, wireframe: true })
 const mesh3 = new THREE.Mesh(geometry, material3)
 mesh3.position.x = 1
 mesh3.position.z = -1
 group.add(mesh3)
 
-const material4 = new THREE.MeshBasicMaterial({ color: 0xffff00 })
+const material4 = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true })
 const mesh4 = new THREE.Mesh(geometry, material4)
 mesh4.position.x = -1
 mesh4.position.z = -1
 group.add(mesh4)
 
-const material5 = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const material5 = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
 const mesh5 = new THREE.Mesh(geometry, material5)
 mesh5.position.x = -1
 group.add(mesh5)
 
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-// camera.lookAt(new THREE.Vector3(1, 0, 0))
 scene.add(camera)
 
 // Renderer
@@ -59,25 +85,17 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 
-const axesHelper = new THREE.AxesHelper(2)
-scene.add(axesHelper)
-
-// Animation
-let time = Date.now()
-
-// animation can also be done using gsap but not in loop, just once ( for now )
-// gsap.to(group.position, { duration: 1, delay: 1, x: 2 })
-// gsap.to(group.position, { duration: 1, delay: 2, x: -2 })
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 const tick = () => {
-    const currentTime = Date.now()
-    const deltaTime = currentTime - time
-    time = currentTime
-
-    group.position.y = Math.sin(time * 0.001)
-    group.position.x = Math.cos(time * 0.001) // sin and cos to make motion like a loop
-    group.rotation.y += 0.0005 * deltaTime // multiplying by deltatime to to make rotation frame rate independent
     
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2
+    // camera.position.y = cursor.y * 3
+    // camera.lookAt(group.position)
+
+    controls.update()
     renderer.render(scene, camera)
     window.requestAnimationFrame(tick)
 }
